c4d4b0b32f33dcc36a6ade2e27ea576599fcff79
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-14 12:14:40 -0500

    Configured Photo View Cell with programmatic constraints.

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,12 +10,102 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    var photo: Photo?
+    private var imageView: UIImageView!
+    private var nameLabel: UILabel!
+    
+    var photo: Photo! {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+    }
+    
+    
     
     
     func setUpSubviews() {
         // Image
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        let imageViewTop = NSLayoutConstraint(item: imageView,
+                                              attribute: .top,
+                                              relatedBy: .equal,
+                                              toItem: safeAreaLayoutGuide,
+                                              attribute: .top,
+                                              multiplier: 1,
+                                              constant: 4)
+        let imageViewLeading = NSLayoutConstraint(item: imageView,
+                                                  attribute: .leading,
+                                                  relatedBy: .equal,
+                                                  toItem: safeAreaLayoutGuide,
+                                                  attribute: .leading,
+                                                  multiplier: 1,
+                                                  constant: 4)
+        let imageViewWidth = NSLayoutConstraint(item: imageView,
+                                                attribute: .width,
+                                                relatedBy: .equal,
+                                                toItem: nil,
+                                                attribute: .notAnAttribute,
+                                                multiplier: 1,
+                                                constant: 150)
+        let imageViewHeight = NSLayoutConstraint(item: imageView,
+                                                 attribute: .height,
+                                                 relatedBy: .lessThanOrEqual,
+                                                 toItem: imageView,
+                                                 attribute: .width,
+                                                 multiplier: 1,
+                                                 constant: 0)
+        
+        NSLayoutConstraint.activate([imageViewTop, imageViewWidth, imageViewHeight, imageViewLeading])
+        
+        self.imageView = imageView
+        
+        // Label
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        
+        NSLayoutConstraint(item: label,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .bottom,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        NSLayoutConstraint(item: label,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: safeAreaLayoutGuide,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        NSLayoutConstraint(item: label,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: safeAreaLayoutGuide,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -4).isActive = true
+        self.nameLabel = label
+        
+    }
+    
+    func updateViews() {
+        
+        imageView.image = UIImage(data: photo.imageData)
+        nameLabel.text = photo.title
     }
 }
